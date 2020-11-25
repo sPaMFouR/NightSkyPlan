37a860ecfa3154e19ecccafdb00d4b51f189d338
Avinash Singh <avinash21292@gmail.com>
2020-11-25 20:11:17 +0530

    Push changes to NightSkyPlan.py

--- NightSkyPlan.py
+++ NightSkyPlan.py
@@ -37,6 +37,9 @@ plt.rc('font', family='sans-serif')
 list_targets = 'TargetList.dat'
 list_telescopes = 'TelescopeList.dat'
 
+dict_twilights = {'Civil': ['-6', True], 'Nautical': ['-12', True], 'Astronomical': ['-18', True],
+                  'Sunset/Sunrise': ['-0.34', False], 'Moonset/Moonrise': ['-0.34', False]}
+
 target_df = pd.read_csv(list_targets, sep='\s+', comment='#').set_index('Index')
 target_df = target_df[target_df['ToPlot'].isin(['y', 'Y'])]
 field_names = ['Object {0}'.format(idx) for idx in target_df.index.values]
@@ -100,9 +103,8 @@ while len(list_values) == 0:
     list_values = eg.multenterbox(msg=err_msg, title=box_title, fields=field_names, values=list_values)
     list_values = remove_empty_values(list_values)
 
-# Plot wrt UTC or Local Time?
-utc = eg.boolbox(msg='Plot Trajectories w.r.t UTC or Local Time?', title='UTC Or Local Time?',
-                 choices=['UTC', 'Local Time'])
+# Plot Trajectories in UTC or Local Time?
+utc = eg.boolbox(msg='Plot Trajectories in UTC or Local Time?', title='Time Zone', choices=['UTC', 'Local Time'])
 
 # Current Date or Manually Entered Date?
 date_obs = str(Time(Time.now(), format='iso', out_subfmt='date'))
@@ -110,9 +112,6 @@ setup_manual = eg.boolbox(msg='Manually Enter Date?', title='Manual or Current D
 
 if setup_manual:
     date_obs = eg.enterbox(msg='Enter The Date Of Observation!', title='Date Of Observation', default=date_obs)
-
-# utc = True
-# setup_manual = True
 # ------------------------------------------------------------------------------------------------------------------- #
 
 
@@ -125,41 +124,83 @@ telescope.lon = OBS_LONG
 telescope.lat = OBS_LAT
 telescope.elevation = OBS_ALT
 telescope.epoch = ephem.J2000
-telescope.date = (Time(date_obs) + 1 * u.day - abs(OBS_TIMEZONE) * u.hour).utc.datetime
+
+time_midnight = (Time(date_obs) + 1 * u.day - abs(OBS_TIMEZONE) * u.hour).datetime
+telescope.date = time_midnight
 # ------------------------------------------------------------------------------------------------------------------- #
 
 
 # ------------------------------------------------------------------------------------------------------------------- #
 # Calculate Sunset, Sunrise and Twilight Times
 # ------------------------------------------------------------------------------------------------------------------- #
-# Calculation Of Local Sunset & Sunrise [Elevation Of Sun = -0.34 Degrees]
-telescope.horizon = '-0:34'
-sunset = telescope.previous_setting(ephem.Sun(), use_center=False)
-sunrise = telescope.next_rising(ephem.Sun(), use_center=False)
 
-# Calculation Of Civil Twilight         [Elevation Of Sun = -6 Degrees]
-telescope.horizon = '-6'
-dusk_civil = telescope.previous_setting(ephem.Sun(), use_center=True)
-dawn_civil = telescope.next_rising(ephem.Sun(), use_center=True)
+def calculate_twilighttime(category='Sunset/Sunrise'):
+    """
+    It will compute Rising and Setting times for the twilight time specified by 'twilight'.
+    Args:
+        category : Specifies which Twilight times have to be coomputed
+    Returns:
+        setting  : Setting time for the twilight
+        rising   : Rising time for the twilight
+    """
+    telescope.horizon = dict_twilights[category][0]
+    setting = telescope.previous_setting(ephem.Sun(), use_center=dict_twilights[category][1])
+    rising = telescope.next_rising(ephem.Sun(), use_center=dict_twilights[category][1])
 
-# Calculation Of Nautical Twilight      [Elevation Of Sun = -12 Degrees]
-telescope.horizon = '-12'
-dusk_nauti = telescope.previous_setting(ephem.Sun(), use_center=True)
-dawn_nauti = telescope.next_rising(ephem.Sun(), use_center=True)
+    return setting, rising
 
-# Calculation Of Astronomical Twilight  [Elevation Of Sun = -18 Degrees]
-telescope.horizon = '-18'
-dusk_astro = telescope.previous_setting(ephem.Sun(), use_center=True)
-dawn_astro = telescope.next_rising(ephem.Sun(), use_center=True)
 
-sunset = Time(datetime.strptime(str(sunset).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-sunrise = Time(datetime.strptime(str(sunrise).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-duskcivil = Time(datetime.strptime(str(dusk_civil).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-dawncivil = Time(datetime.strptime(str(dawn_civil).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-dusknauti = Time(datetime.strptime(str(dusk_nauti).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-dawnnauti = Time(datetime.strptime(str(dawn_nauti).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-duskastro = Time(datetime.strptime(str(dusk_astro).split('.')[0], '%Y/%m/%d %H:%M:%S'))
-dawnastro = Time(datetime.strptime(str(dawn_astro).split('.')[0], '%Y/%m/%d %H:%M:%S'))
+def calculate_moontime(time_midnight):
+    """
+    It will compute Moonrise and Moonset times.
+    Args:
+        time_midnight : Midnight time for the date on which Moonrise and Moonset is to be computed
+    Returns:
+        rising        : Time of Moonrise
+        setting       : Time of Moonset
+    """
+    category = 'Moonset/Moonrise'
+    telescope.horizon = dict_twilights[category][0]
+
+    previousrise = telescope.previous_rising(ephem.Moon(), use_center=dict_twilights[category][1])
+    previousset = telescope.previous_setting(ephem.Moon(), use_center=dict_twilights[category][1])
+    nextset = telescope.next_setting(ephem.Moon(), use_center=dict_twilights[category][1])
+    nextrise = telescope.next_rising(ephem.Moon(), use_center=dict_twilights[category][1])
+
+    if previousrise.datetime() > previousset.datetime():
+        rising = previousrise
+        setting = nextset
+    if time_midnight - previousset.datetime() > nextrise.datetime() - time_midnight:
+        rising = nextrise
+        setting = nextset
+    else:
+        rising = previousrise
+        setting = previousset
+
+    return rising, setting
+
+
+# Calculation Of Local MoonRise & MoonSet [Refraction Correction Of Moon = -0.34 Degrees]
+moon_rise, moon_set = calculate_moontime(time_midnight)
+
+# Calculation Of Local Sunset & Sunrise [Refractrion Correction for Sun = -0.34 Degrees]
+sun_set, sun_rise = calculate_twilighttime('Sunset/Sunrise')
+
+# Calculation Of Civil Twilight [Elevation Of Sun = -6 Degrees]
+dusk_civil, dawn_civil = calculate_twilighttime('Civil')
+
+# Calculation Of Nautical Twilight [Elevation Of Sun = -12 Degrees]
+dusk_nauti, dawn_nauti = calculate_twilighttime('Nautical')
+
+# Calculation Of Astronomical Twilight [Elevation Of Sun = -18 Degrees]
+dusk_astro, dawn_astro = calculate_twilighttime('Astronomical')
+
+twilighttimes = [Time(datetime.strptime(str(time).split('.')[0], '%Y/%m/%d %H:%M:%S'))
+                 for time in [sun_set, dusk_civil, dusk_nauti, dusk_astro, dawn_astro, 
+                              dawn_nauti, dawn_civil, sun_rise, moon_rise, moon_set]]
+
+(sunset, duskcivil, dusknauti, duskastro, dawnastro, dawnnauti,
+                               dawncivil, sunrise, moonrise, moonset) = twilighttimes
 # ------------------------------------------------------------------------------------------------------------------- #
 
 
@@ -296,28 +337,29 @@ def plot_obsplan(ax_obj, utc=True):
 
     # Print Text In The Plot
     # ------------------------------------------------------------------------------------------------------------- #
-    ax_obj.text(sunset.value, 92, 'Sunset', rotation=50, c='orangered', fontsize=10)
-    ax_obj.text(sunrise.value - timedelta(minutes=10), 92, 'Sunrise', rotation=+50, c='orangered', fontsize=10)
-    ax_obj.text(duskcivil.value, 7, 'Civil Twilight', rotation=-90, c='navy', alpha=1, fontsize=10)
-    ax_obj.text(dawncivil.value, 7, 'Civil Twilight', rotation=-90, c='navy', alpha=1, fontsize=10)
-    ax_obj.text(dusknauti.value, 5, 'Nautical Twilight', rotation=-90, c='navy', alpha=1, fontsize=10)
-    ax_obj.text(dawnnauti.value, 5, 'Nautical Twilight', rotation=-90, c='navy', alpha=1, fontsize=10)
-    ax_obj.text(duskastro.value, 3, 'Astronomical Twilight', rotation=-90, c='navy', alpha=1, fontsize=10)
-    ax_obj.text(dawnastro.value, 3, 'Astronomical Twilight', rotation=-90, c='navy', alpha=1, fontsize=10)
+    ax_obj.text(sunset.value, 91, 'Sunset', rotation=50, c='navy', fontsize=9)
+    ax_obj.text(sunrise.value - timedelta(minutes=10), 91, 'Sunrise', rotation=+50, c='navy', fontsize=9)
+    ax_obj.text(duskcivil.value, 3, 'Civil', rotation=-90, c='navy', alpha=1, fontsize=9)
+    ax_obj.text(dawncivil.value, 3, 'Civil', rotation=-90, c='navy', alpha=1, fontsize=9)
+    ax_obj.text(dusknauti.value, 3, 'Nautical', rotation=-90, c='navy', alpha=1, fontsize=9)
+    ax_obj.text(dawnnauti.value, 3, 'Nautical', rotation=-90, c='navy', alpha=1, fontsize=9)
+    ax_obj.text(duskastro.value, 3, 'Astronomical', rotation=-90, c='navy', alpha=1, fontsize=9)
+    ax_obj.text(dawnastro.value, 3, 'Astronomical', rotation=-90, c='navy', alpha=1, fontsize=9)
 
     nightspan = dawnnauti.value - dusknauti.value
     midnight = dusknauti.value + nightspan / 2
     printtime = datetime.strptime(str(currenttime).split('.')[0], '%Y-%m-%d %H:%M:%S')
     printnightspan = 'Night Span = {0}'.format(str(nightspan))
     printmoonphase = 'Moon Illumination = {0:.1f}%'.format(ephem.Moon(midnight).phase)
+    printdate = 'Date of Observation : {0}'.format(date_obs)
 
-    ax_obj.text(midnight - timedelta(minutes=25), HORIZON - 3, 'Telescope Horizon', c='navy', fontsize=10)
-    ax_obj.text(midnight - timedelta(minutes=25), ZENITH + 1, 'Telescope Zenith', c='navy', fontsize=10)
+    ax_obj.text(midnight - timedelta(minutes=25), HORIZON - 2, 'Telescope Horizon', c='navy', fontsize=9)
+    ax_obj.text(midnight - timedelta(minutes=25), ZENITH + 1, 'Telescope Zenith', c='navy', fontsize=9)
     ax_obj.text(midnight - timedelta(hours=2), 91, s=printnightspan + ', ' + printmoonphase, c='r', fontsize=12)
 
     if sunset.value < currenttime.utc.datetime < sunrise.value:
-        ax_obj.axvline(x=currenttime.value, ls='--', color='k')
-        ax_obj.text(currenttime.value, 7, 'Current Time', rotation=-90, c='k', fontsize=10)
+        ax_obj.axvline(x=currenttime.value, ls='--', lw=1, color='red')
+        ax_obj.text(currenttime.value, 3, 'Current Time', rotation=-90, c='red', fontsize=10)
     # ------------------------------------------------------------------------------------------------------------- #
 
     # Fill Color In Sectors of Observation/Non-Observation
@@ -352,7 +394,7 @@ def plot_obsplan(ax_obj, utc=True):
     ax_obj.yaxis.set_minor_locator(FixedLocator(range(0, 91, 2)))
     ax_obj.xaxis.set_minor_locator(MinuteLocator(byminute=range(0, 60, 10)))
     ax_obj.xaxis.set_major_formatter(DateFormatter('%H:%M'))
-    ax_obj.tick_params(axis='both', which='major', direction='in', width=1.6, length=9, labelsize=12)
+    ax_obj.tick_params(axis='both', which='major', direction='in', width=1.6, length=8, labelsize=12)
     ax_obj.tick_params(axis='both', which='minor', direction='in', width=0.9, length=5, labelsize=12)
     # ------------------------------------------------------------------------------------------------------------- #
 
@@ -370,7 +412,7 @@ def plot_obsplan(ax_obj, utc=True):
     ax_twin.set_yticklabels(list_secz)
     ax_twin.set_yticks(ax_obj.get_yticks())
     ax_twin.set_yticks(ax_obj.get_yticks(minor=True), minor=True)
-    ax_twin.tick_params(axis='both', which='major', direction='in', width=1.6, length=9, labelsize=12)
+    ax_twin.tick_params(axis='both', which='major', direction='in', width=1.6, length=8, labelsize=12)
     ax_twin.tick_params(axis='both', which='minor', direction='in', width=0.9, length=5, labelsize=12)
     # ------------------------------------------------------------------------------------------------------------- #
 
@@ -386,9 +428,9 @@ def plot_obsplan(ax_obj, utc=True):
     ax_twin.set_ylabel('Airmass', fontsize=16)
 
     if not utc:
-        ax_obj.set_xlabel('\nLocal Time [In Hours]\nCurrent Time : ' + str(printtime) + ' UT', fontsize=16)
+        ax_obj.set_xlabel('\nLocal Time [In Hours]\n' + str(printdate), fontsize=16)
     else:
-        ax_obj.set_xlabel('\nUniversal Time [In Hours]\nCurrent Time : ' + str(printtime) + ' UT', fontsize=16)
+        ax_obj.set_xlabel('\nUniversal Time [In Hours]\n' + str(printdate), fontsize=16)
     # ------------------------------------------------------------------------------------------------------------- #
 
     # Show and Save the Plot
